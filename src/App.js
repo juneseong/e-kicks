5bd43e687c546289dc94f6b09495e7630a88c6dd
June <jyjseong@gmail.com>
2020-06-30 11:42:55 -0400

    updated shop page with collection preview component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route } from 'react-router-dom';
 import './App.css';
 import './reset.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.component.jsx';
 
 function App() {
   return (
     <div>
       <Route exact path='/' component={HomePage} />
+      <Route path='/shop' component={ShopPage} />
     </div>
   );
 }
---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,171 @@
+const SHOP_DATA = [
+    {
+        id: 1,
+        title: 'Nike',
+        routeName: 'nike',
+        items: [
+            {
+                id: 1,
+                name: 'Nike Air Force 270',
+                imageUrl: 'images/shoes/nike1.jpg',
+                price: 225
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: 'Reebok',
+        routeName: 'reebok',
+        items: [
+            {
+                id: 2,
+                name: 'Reebok Workout Plus',
+                imageUrl: 'images/shoes/reebok1.jpg',
+                price: 220
+            },
+            {
+                id: 3,
+                name: 'Reebok Workout Plus',
+                imageUrl: 'images/shoes/reebok2.jpg',
+                price: 280
+            },
+            {
+                id: 4,
+                name: 'Reebok Aztrek',
+                imageUrl: 'images/shoes/reebok3.jpg',
+                price: 110
+            },
+            {
+                id: 5,
+                name: 'Reebok Aztrek',
+                imageUrl: 'images/shoes/reebok4.jpg',
+                price: 160
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: 'Adidas',
+        routeName: 'adidas',
+        items: [
+            {
+                id: 6,
+                name: 'Adidas Sambarose',
+                imageUrl: 'images/shoes/adidas1.jpg',
+                price: 125
+            },
+            {
+                id: 7,
+                name: 'Adidas Sambarose',
+                imageUrl: 'images/shoes/adidas2.jpg',
+                price: 190
+            },
+            {
+                id: 8,
+                name: 'Adidas Campus',
+                imageUrl: 'images/shoes/adidas3.jpg',
+                price: 190
+            },
+            {
+                id: 9,
+                name: 'Adidas Campus',
+                imageUrl: 'images/shoes/adidas4.jpg',
+                price: 165
+            },
+            {
+                id: 10,
+                name: 'Adidas Adilette Shower',
+                imageUrl: 'images/shoes/adidas5.jpg',
+                price: 185
+            },
+            {
+                id: 11,
+                name: 'Adidas Adilette Shower',
+                imageUrl: 'images/shoes/adidas6.jpg',
+                price: 185
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: 'Womens',
+        routeName: 'womens',
+        items: [
+            {
+                id: 2,
+                name: 'Reebok Workout Plus',
+                imageUrl: 'images/shoes/reebok1.jpg',
+                price: 220
+            },
+            {
+                id: 3,
+                name: 'Reebok Workout Plus',
+                imageUrl: 'images/shoes/reebok2.jpg',
+                price: 280
+            },
+            {
+                id: 6,
+                name: 'Adidas Sambarose',
+                imageUrl: 'images/shoes/adidas1.jpg',
+                price: 125
+            },
+            {
+                id: 7,
+                name: 'Adidas Sambarose',
+                imageUrl: 'images/shoes/adidas2.jpg',
+                price: 190
+            },
+            {
+                id: 8,
+                name: 'Adidas Campus',
+                imageUrl: 'images/shoes/adidas3.jpg',
+                price: 190
+            },
+            {
+                id: 9,
+                name: 'Adidas Campus',
+                imageUrl: 'images/shoes/adidas4.jpg',
+                price: 165
+            }
+        ]
+    },
+    {
+        id: 5,
+        title: 'Mens',
+        routeName: 'mens',
+        items: [
+            {
+                id: 1,
+                name: 'Nike Air Force 270',
+                imageUrl: 'images/shoes/nike1.jpg',
+                price: 225
+            },
+            {
+                id: 4,
+                name: 'Reebok Aztrek',
+                imageUrl: 'images/shoes/reebok3.jpg',
+                price: 110
+            },
+            {
+                id: 5,
+                name: 'Reebok Aztrek',
+                imageUrl: 'images/shoes/reebok4.jpg',
+                price: 160
+            },
+            {
+                id: 10,
+                name: 'Adidas Adilette Shower',
+                imageUrl: 'images/shoes/adidas5.jpg',
+                price: 185
+            },
+            {
+                id: 11,
+                name: 'Adidas Adilette Shower',
+                imageUrl: 'images/shoes/adidas6.jpg',
+                price: 185
+            }
+        ]
+    }
+];
+
+export default SHOP_DATA;

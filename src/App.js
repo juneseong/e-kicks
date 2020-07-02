bc2ac40bcc63816692ad76ad46caac99046fe6b8
June <jyjseong@gmail.com>
2020-07-02 10:48:37 -0400

    updated firebase utils to store authenticated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -24,6 +24,8 @@ class App extends React.Component {
               ...snapShot.data()
             }
           });
+
+          console.log(this.state);
         });
       } else {
         this.setState({ currentUser: userAuth });

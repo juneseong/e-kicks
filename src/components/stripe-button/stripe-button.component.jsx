b05c4521fadda882812d6cddd7b1c21b59e4f9f2
June <jyjseong@gmail.com>
2020-07-07 17:48:29 -0400

    updated stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51H2OHTIjBMU2RU4Q7dryx3yQBojrZhTh1IxfBqiqdaRcXrjlCGIcEokHJLBJxtJm9vau8QoepOyxXepfQjnIgyjI00CA8nlPN4';
+
+    const onToken = () => {
+        alert('Payment Successful');
+    };
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='e-kicks'
+            billingAddress
+            shippingAddress
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+};
+
+export default StripeCheckoutButton;
